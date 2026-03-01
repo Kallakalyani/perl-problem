19e39253a9e3a5ac2765a45eb122a73e74de8c66
Kallakalyani <154115308+Kallakalyani@users.noreply.github.com>
2026-03-01 20:55:53 +0530

    Add files via upload

---
+++ variable.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+# vector variables
+my $names = ("teju");
+my $age = 20;
+my $salary = 5555.55;
+print "$names\n";
+print "$age\n";
+print "$salary\n";
+#print "size of the array: ".scalar(@names);
